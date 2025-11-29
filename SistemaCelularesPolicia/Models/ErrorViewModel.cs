08938b5cfb2111036f41f7b1a84be497fb1e8e17
TFair <109878320+xTFair@users.noreply.github.com>
2025-11-28 19:50:38 -0500

    Agregar archivos de proyecto.

---
+++ SistemaCelularesPolicia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaCelularesPolicia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
